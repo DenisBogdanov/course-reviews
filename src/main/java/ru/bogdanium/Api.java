88cbec2138685027482a9beb1312ad480eaddd5e
Denis Bogdanov <bogdanov.math@gmail.com>
2018-04-04 14:11:54 +0300

    Fix course request by id.

--- src/main/java/ru/bogdanium/Api.java
+++ src/main/java/ru/bogdanium/Api.java
@@ -11,7 +11,8 @@ import static spark.Spark.*;
 public class Api {
     public static void main(String[] args) {
 
-        Sql2o sql2o = new Sql2o("jdbc:h2:~/reviews.db; INIT=RUNSCRIPT from 'classpath:db/init.sql'");
+        Sql2o sql2o = new Sql2o("jdbc:h2:C:\\Users\\Denis\\Dropbox\\Projects\\sparkjava\\course-reviews\\data\\reviews.db;" +
+                "INIT=RUNSCRIPT from 'classpath:db/init.sql'", "", "");
         CourseDao courseDao = new Sql2oCourseDao(sql2o);
         Gson gson = new Gson();
 
@@ -22,12 +23,15 @@ public class Api {
             return course;
         }, gson::toJson);
 
-        get("/courses", "application/json", (req, res) -> courseDao.findAll(), gson::toJson);
+        get("/courses", "application/json",
+                (req, res) -> courseDao.findAll(), gson::toJson);
 
         get("/courses/:id", "applications/json", (req, res) -> {
             int id = Integer.parseInt(req.params("id"));
-            return null;
-        });
+            // TODO: What if this is not found?
+            Course course = courseDao.findById(id);
+            return course;
+        }, gson::toJson);
 
         after((req, res) -> {
             res.type("application/json");
--- src/main/java/ru/bogdanium/dao/Sql2oCourseDao.java
+++ src/main/java/ru/bogdanium/dao/Sql2oCourseDao.java
@@ -10,6 +10,10 @@ import java.util.List;
 
 public class Sql2oCourseDao implements CourseDao {
 
+    private static final String SQL_ADD_COURSE = "INSERT INTO courses(name, url) VALUES (:name, :url)";
+    private static final String SQL_GET_ALL_COURSES = "SELECT * FROM courses";
+    private static final String SQL_GET_COURSE_BY_ID = "SELECT * FROM courses WHERE id = :id";
+
     private final Sql2o sql2o;
 
     public Sql2oCourseDao(Sql2o sql2o) {
@@ -19,33 +23,35 @@ public class Sql2oCourseDao implements CourseDao {
     @Override
     public void add(Course course) throws DaoException {
 
-        String sql = "INSERT INTO courses(name, url) VALUES (:name, :url)";
         try (Connection connection = sql2o.open()) {
-            int key = (int) connection.createQuery(sql)
+            int key = (int) connection
+                    .createQuery(SQL_ADD_COURSE)
                     .bind(course)
                     .executeUpdate()
                     .getKey();
+
             course.setId(key);
 
         } catch (Sql2oException e) {
             throw new DaoException(e, "Problem adding course");
         }
-
     }
 
     @Override
     public List<Course> findAll() {
 
-        try (Connection connection = sql2o.open()){
-            return connection.createQuery("SELECT * FROM courses")
+        try (Connection connection = sql2o.open()) {
+            return connection
+                    .createQuery(SQL_GET_ALL_COURSES)
                     .executeAndFetch(Course.class);
         }
     }
 
     @Override
     public Course findById(int id) {
-        try (Connection connection = sql2o.open()){
-            return connection.createQuery("SELECT * FROM courses WHERE id = :id")
+        try (Connection connection = sql2o.open()) {
+            return connection
+                    .createQuery(SQL_GET_COURSE_BY_ID)
                     .addParameter("id", id)
                     .executeAndFetchFirst(Course.class);
         }
